da0868de7dbe360011d1bc0fda40dca70523678a
extrowerk <zmizsei@extrowerk.com>
2025-11-13 06:45:04 +0000

    OpenIndiana patches from iigs

--- src/unix/solarish/mod.rs
+++ src/unix/solarish/mod.rs
@@ -2390,6 +2390,9 @@ const NEWDEV: c_int = 1;
 // sys/sendfile.h
 pub const SFV_FD_SELF: c_int = -2;
 
+// command names for confstr sys/unistd.h
+pub const _CS_PATH: c_int = 65;
+
 const fn _CMSG_HDR_ALIGN(p: usize) -> usize {
     (p + _CMSG_HDR_ALIGNMENT - 1) & !(_CMSG_HDR_ALIGNMENT - 1)
 }
@@ -2756,6 +2759,7 @@ extern "C" {
         addrlen: *mut crate::socklen_t,
     ) -> ssize_t;
     pub fn mkstemps(template: *mut c_char, suffixlen: c_int) -> c_int;
+    pub fn mkostemp(template: *mut c_char, flags: c_int) -> c_int;
     pub fn futimesat(fd: c_int, path: *const c_char, times: *const crate::timeval) -> c_int;
     pub fn futimens(dirfd: c_int, times: *const crate::timespec) -> c_int;
     pub fn utimensat(
